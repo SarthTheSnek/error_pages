27fd65239589ee3ea614de8a27aad829a643ddd6
SarthTheSnek <34319569+SarthTheSnek@users.noreply.github.com>
2021-06-06 22:27:53 -0700

    Updating build command and compile script

--- fastestfurs/compile.js
+++ fastestfurs/compile.js
@@ -1,8 +1,8 @@
 const pug = require('pug');
 const fs = require('fs');
 
-let html = pug.renderFile('./error_500.pug');
-fs.writeFile('./error_500.html', html, err => {
+let html = pug.renderFile('./500.pug');
+fs.writeFile('./500.html', html, err => {
     if (err) {
         console.error(err)
         return
