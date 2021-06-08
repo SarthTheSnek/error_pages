88851e2f8b3165fec35437ed1b7bf3479ec5bbd9
SarthTheSnek <34319569+SarthTheSnek@users.noreply.github.com>
2021-06-07 20:55:23 -0700

    Updating compile commands with new error pages

--- compile.js
+++ compile.js
@@ -1,8 +1,24 @@
 const pug = require('pug');
 const fs = require('fs');
 
-let html = pug.renderFile('./500.pug');
-fs.writeFile('./500.html', html, err => {
+let html = pug.renderFile('./400.pug');
+fs.writeFile('./400.html', html, err => {
+    if (err) {
+        console.error(err)
+        return
+    }
+});
+
+html = pug.renderFile('./401.pug');
+fs.writeFile('./401.html', html, err => {
+    if (err) {
+        console.error(err)
+        return
+    }
+});
+
+html = pug.renderFile('./403.pug');
+fs.writeFile('./403.html', html, err => {
     if (err) {
         console.error(err)
         return
@@ -17,8 +33,40 @@ fs.writeFile('./404.html', html, err => {
     }
 });
 
-html = pug.renderFile('./403.pug');
-fs.writeFile('./403.html', html, err => {
+html = pug.renderFile('./405.pug');
+fs.writeFile('./405.html', html, err => {
+    if (err) {
+        console.error(err)
+        return
+    }
+});
+
+html = pug.renderFile('./418.pug');
+fs.writeFile('./418.html', html, err => {
+    if (err) {
+        console.error(err)
+        return
+    }
+});
+
+html = pug.renderFile('./500.pug');
+fs.writeFile('./500.html', html, err => {
+    if (err) {
+        console.error(err)
+        return
+    }
+});
+
+html = pug.renderFile('./502.pug');
+fs.writeFile('./500.html', html, err => {
+    if (err) {
+        console.error(err)
+        return
+    }
+});
+
+html = pug.renderFile('./503.pug');
+fs.writeFile('./503.html', html, err => {
     if (err) {
         console.error(err)
         return
